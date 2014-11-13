4a135caae895a60d272c5f3fc688e427ffd23108
Eugene Sia <eugene.sia@ticketmaster.co.uk>
2014-11-13 04:31:25 +0000

    Add checkAnswer(boolean) #2.10

--- src/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ src/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -32,6 +32,21 @@ public class QuizActivity extends ActionBarActivity {
         mQuestionTextView.setText(question);
     }
     
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isTrueQuestion();
+        
+        int messageResId = 0;
+        
+        if (userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+            .show();
+    }
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

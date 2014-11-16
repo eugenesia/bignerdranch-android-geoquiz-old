928a8b84e7e567985286e995efdf96fb29b4252b
Eugene Sia <eugene@eugenesia.co.uk>
2014-11-16 05:07:49 +0000

    Set a result #5.14

--- src/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ src/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.geoquiz;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,11 +11,20 @@ public class CheatActivity extends Activity {
     
     public static final String EXTRA_ANSWER_IS_TRUE =
         "com.bignerdranch.android.geoquiz.answer_is_true";
+    public static final String EXTRA_ANSWER_SHOWN =
+        "com.bignerdranch.android.geoquiz.answer_shown";
 
     private boolean mAnswerIsTrue;
     
     private TextView mAnswerTextView;
     private Button mShowAnswer;
+    
+
+    private void setAnswerShownResult(boolean isAnswerShown) {
+        Intent data = new Intent();
+        data.putExtra(EXTRA_ANSWER_SHOWN, isAnswerShown);
+        setResult(RESULT_OK, data);
+    }
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +36,7 @@ public class CheatActivity extends Activity {
         mAnswerTextView = (TextView)findViewById(R.id.answerTextView);
         
         mShowAnswer = (Button)findViewById(R.id.showAnswerButton);
-        mShowAnswer.setOnClickListener(new View.OnClickListener() {
-            
+        mShowAnswer.setOnClickListener(new View.OnClickListener() {            
             @Override
             public void onClick(View v) {
                 if (mAnswerIsTrue) {
@@ -35,6 +44,7 @@ public class CheatActivity extends Activity {
                 } else {
                     mAnswerTextView.setText(R.string.false_button);
                 }
+                setAnswerShownResult(true);
             }
         });
     }

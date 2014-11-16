87324ccafaf066abdbcee68ed717fc54b2b53b6a
Eugene Sia <eugene@eugenesia.co.uk>
2014-11-16 04:52:35 +0000

    Put an extra on the intent #5.10

--- src/com/bignerdranch/android/geoquiz/CheatActivity.java
+++ src/com/bignerdranch/android/geoquiz/CheatActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 
 public class CheatActivity extends Activity {
     
+    public static final String EXTRA_ANSWER_IS_TRUE =
+        "com.bignerdranch.android.geoquiz.answer_is_true";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
